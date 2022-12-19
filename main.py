14b22431306c07785bf387d7c8e0d16d54c96281
MKinga08 <dr.m.kinga08@gmail.com>
2022-12-19 10:03:35 +0100

    refactor and print game board

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 def game_board():
-    rows, cols = (5, 5)
-    board = [[0] * cols] * rows
-    for row in board:
-        print(row)
+    board = []
+    rows, cols = 5, 5
+    print("   A", " B", " C", " D", " E")
+    for i in range(rows):
+        col = []
+        for j in range(cols):
+            col.append("0")
+        board.append(col)
+    return board
+
+
+def print_gameboard(board):
+    counter = 0
+    for i in board:
+        collector = " "
+        counter += 1
+        for k in i:
+            collector += k + "  "
+        print(counter, collector)
 
 
 def main():
-    game_board()
+    gboard = game_board()
+    print_gameboard(gboard)
 
 
 if __name__ == "__main__":
